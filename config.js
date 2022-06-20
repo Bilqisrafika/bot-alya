930549107c00e67b71eda5510ca85fd25409afc8
X - XDZ <104031305+Bilqisrafika@users.noreply.github.com>
2022-06-20 13:45:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Other
+global.owner = ['6285334190704']
+global.premium = ['6285334190704']
+global.ownername = 'Ditz'
+global.botname = 'Exs - Botz'
+global.packname = '© by ExsBotz'
+global.gc = 'https://bit.ly/3KMlmv2'
+global.linkyt = 'https://youtube.com/channel/UCjdHhl4_n8k9s4I90fvTx0g'
+global.linkgc = 'https://chat.whatsapp.com/Dupj9RhYXykDOAAiQoM0GG'
+global.limitawal = '100'
+global.author = '@aditadwir_'
+global.sessionName = 'polo'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = 'terserah'
+global.mess = {
+    success: 'Berhasil',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Tunggu sebentar, sedang di proses',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.sc = fs.readFileSync('./media/sc.jpg')
+global.tq = fs.readFileSync('./media/tq.jpg')
+global.menu = fs.readFileSync('./media/menu.jpg')
+global.allmenu = fs.readFileSync('./media/allmenu.jpg')
+global.thumb = { url: 'https://i.pinimg.com/736x/d0/f8/b8/d0f8b804a908ce4aaee63d54035d2192.jpg' }
+global.visoka = { url: 'https://telegra.ph/file/769d88bb8c8357546a149.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
